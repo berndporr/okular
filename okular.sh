98434b743cb2d76e74cea89771aebf0db106fe53
Bernd Porr <mail@berndporr.me.uk>
2020-08-13 20:32:57 +0100

    Added file for profile.d

---
+++ okular.sh
@@ -0,0 +1,4 @@
+
+export XDG_DATA_DIRS=/usr/local/share:${XDG_DATA_DIRS:-/usr/local/share/:/usr/share/}
+export QT_PLUGIN_PATH=/usr/local/lib/x86_64-linux-gnu/plugins/:$QT_PLUGIN_PATH
+export QML2_IMPORT_PATH=/usr/local/lib/x86_64-linux-gnu/qml/:$QML2_IMPORT_PATH
